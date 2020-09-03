163fa42f1b25086cc909e0684472e28aee9a1f28
Kelvin Kang <kelvinkang@cmu.edu>
2020-09-03 15:03:46 +0800

    fix euler and midpoint method, add class abstraction for motionmodel

--- Ex1_1_EulerMethod.m
+++ Ex1_1_EulerMethod.m
@@ -1,20 +1,42 @@
 % time step
-T = 0.01;
+T = 0.1;
 % ending time
 tf = 20;
-% initial condition
 numOfIteration = ceil(tf/T);
-miu = 0.2;
-motionFunc = Ex1_MotionFunction;
+% spring-mass dampener system variables
+m = 1;
+ks = 1;
+miu = 0;
+u = 0;
+% instantiate object for motion modelling
+motionFunc = Ex1_MotionFunction(m, ks ,miu, u);
 
+% create matrix placeholder for Euler method and midpoint method
 x = zeros(2, numOfIteration);
+x_mid = zeros(2, numOfIteration);
 t = zeros(1, numOfIteration);
-x(:,1) = [1; 0]; % x stores all the state vector in the time series
+
+% init starting state
+x(:,1) = [1; 0];
+x_mid(:,1) = [1; 0];
 t(1) = 0;
 
+% loop over for numerical integration
 for k = 1:numOfIteration
-    x(:,k+1) = x(:,k) + T*motionFunc.f_damping(x(:,k), miu);
+    % Euler Method
+    x(:,k+1) = x(:,k) + T*motionFunc.f(x(:,k));
+    % Midpoint method
+    x_mid(:,k+1) = x_mid(:,k) + T*motionFunc.f(x_mid(:,k)+ ...
+        0.5*T*motionFunc.f(x_mid(:,k)));
+    % update time
     t(k+1) = t(k) + T;
 end
 
-plot(t, x(1,:));
+groundTruth = cos(t);
+plot(t, groundTruth);
+hold on;
+scatter(t, x(1,:));
+hold on;
+scatter(t, x_mid(1,:));
+legend({'Ground Truth', 'Euler Method', 'Midpoint Method'});
+hold off;
--- Ex1_MotionFunction.m
+++ Ex1_MotionFunction.m
@@ -1,5 +1,20 @@
 classdef Ex1_MotionFunction
+    properties
+        miu_;
+        m_;
+        k_;
+        u_;
+    end
     methods
+        function obj = Ex1_MotionFunction(m, k, miu, u)
+            obj.m_ = m;
+            obj.k_ = k;
+            obj.miu_ = miu;
+            obj.u_ = u;
+        end
+        function x_dot = f(obj, x)
+           x_dot = [x(2); (obj.u_ - obj.k_*x(1) - obj.miu_*x(2))./obj.m_];
+        end
         function x_dot = f_lossless(~, x)
             x_dot = [x(2); -x(1)];
         end
